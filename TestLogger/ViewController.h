2cdf992d580ca0145f859b03878b28402d784545
zhonggaorong <458673541@qq.com>
2016-06-13 11:18:13 +0800

    Initial Commit

---
+++ TestLogger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestLogger
+//
+//  Created by 张国荣 on 16/6/13.
+//  Copyright © 2016年 BateOrganization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestLogger/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestLogger
+//
+//  Created by 张国荣 on 16/6/13.
+//  Copyright © 2016年 BateOrganization. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
